42efd059036458701829cc42c05a0a90a2dac0aa
Jakub Wojcik <j.wojcik1994@gmail.com>
2022-02-06 20:45:51 +0100

    Add validation function, clear function. Finish logic on handlers

--- ex2/js/app.js
+++ ex2/js/app.js
@@ -2,9 +2,10 @@
 
 const modalElement = document.getElementById("modal");
 const modalTimeElement = document.getElementById("modal_time");
+const mainElement = document.getElementById("main_app");
 const formElement = document.querySelector(".form-grid");
-const inputPhoneWrapper = document.querySelector(".input-group-phone");
-const inputCodeWrapper = document.querySelector(".input-group-code");
+const errorMsgPhoneElement = document.querySelector(".error-msg-phone");
+const errorMsgCodeElement = document.querySelector(".error-msg-code");
 const inputPhoneElement = document.getElementById("input_phone");
 const inputCodeElement = document.getElementById("input_code");
 const btnModalFinish = document.querySelector(".btn-finish");
@@ -16,13 +17,24 @@ const btnFormSubmit = document.querySelector(".btn-submit");
 let timerStarted = false;
 let timeAmount = 0;
 let timerCount;
+let inputPhoneNumber = "";
+let inputCode = "";
 
 // database
 
-const db = [
-  { phoneNumber: 123456789, code: 1234 },
-  { phoneNumber: 987654321, code: 4321 },
-  { phoneNumber: 666666666, code: 6666 },
+const DATA_BASE = [
+  {
+    userPhoneNumber: "111111111",
+    userCode: "1111",
+  },
+  {
+    userPhoneNumber: "222222222",
+    userCode: "2222",
+  },
+  {
+    userPhoneNumber: "123456789",
+    userCode: "1234",
+  },
 ];
 
 //Init App
@@ -30,8 +42,8 @@ const appInit = () => {
   btnFormSubmit.addEventListener("click", handleSubmit);
   btnModalFinish.addEventListener("click", handleFinish);
   btnModalRestart.addEventListener("click", handleRestart);
-  inputPhoneElement.addEventListener("input", handleChangeInputs);
-  inputCodeElement.addEventListener("input", handleChangeInputs);
+  inputPhoneElement.addEventListener("input", handleChangePhoneNumber);
+  inputCodeElement.addEventListener("input", handleChangeCode);
 };
 
 //Submit button handler
@@ -42,76 +54,79 @@ const handleSubmit = (e) => {
     timerStarted = !timerStarted;
     timerStart();
   } else {
-    modalElement.classList.add("active");
-    timerStop();
+    validate(inputPhoneNumber, inputCode);
   }
 };
-//Inputs handler
-const handleChangeInputs = (e) => {
-  const { name, value } = e.target;
 
-  values = {
-    ...values,
-    [name]: value,
-  };
-
-  console.log("values:", values);
+//Inputs handlers
+const handleChangePhoneNumber = (e) => {
+  console.log("phone:", e.target.value);
+  return (inputPhoneNumber = e.target.value);
 };
 
-let values = { phoneNumber: "", code: "" };
+const handleChangeCode = (e) => {
+  console.log("code:", e.target.value);
+  return (inputCode = e.target.value);
+};
 
 //Validate
 
-const validate = (values) => {
+const isInDatabase = (phoneNumber, code) => {
+  DATA_BASE.forEach((record) => {
+    if (record.userPhoneNumber === phoneNumber && record.userCode === code) {
+      mainElement.classList.remove("active");
+      timerStop();
+      modalElement.classList.add("active");
+    } else {
+      errorMsgPhoneElement.classList.add("active");
+      errorMsgCodeElement.classList.add("active");
+      const errorNotInDatabase = "Podanej kombinacji nie znaleziono w bazie";
+      errorMsgPhoneElement.innerText = errorNotInDatabase;
+      errorMsgCodeElement.innerText = errorNotInDatabase;
+    }
+  });
+};
+
+const validate = (phoneNumber, code) => {
   let errors = {};
-  let phoneNumberErrorMsgWrapper = null;
-  let codeErrorMsgWrapper = null;
 
-  //=====================PHONE NUMBER===========================================
-  db.map((item) => {
-    if (item.phoneNumber === values.phoneNumber) {
-      return;
-    } else if (item.phoneNumber !== values.phoneNumber) {
-      errors.phoneNumber = "Podaj prawidłowy numer telefonu";
-      if (phoneNumberErrorMsgWrapper === null) {
-        phoneNumberErrorMsgWrapper = inputPhoneWrapper.createElement("div");
-        phoneNumberErrorMsgWrapper.innerHTML = `<p class="error-msg">${errors.phoneNumber}</p>`;
-      } else {
-        phoneNumberErrorMsgWrapper.innerHTML = `<p class="error-msg">${errors.phoneNumber}</p>`;
-      }
-    } else if (values.phoneNumber.trim().length === 0) {
-      errors.phoneNumber = "Proszę wprowadzić 9-cyfrowy numer telefonu";
-      if (phoneNumberErrorMsgWrapper === null) {
-        phoneNumberErrorMsgWrapper = inputPhoneWrapper.createElement("div");
-        phoneNumberErrorMsgWrapper.innerHTML = `<p class="error-msg">${errors.phoneNumber}</p>`;
-      } else {
-        phoneNumberErrorMsgWrapper.innerHTML = `<p class="error-msg">${errors.phoneNumber}</p>`;
-      }
-    }
-  });
-  //=====================CODE===========================================
+  if (phoneNumber.length !== 9) {
+    errors.phoneNumber = "Wprowadź 9-cyfrowy numer telefonu";
+    errorMsgPhoneElement.classList.add("active");
+    errorMsgPhoneElement.innerText = errors.phoneNumber;
+  } else if (isNaN(+phoneNumber)) {
+    errorMsgPhoneElement.classList.add("active");
+    errors.phoneNumber = "Numer musi składać się z cyfr";
+    errorMsgPhoneElement.innerText = errors.phoneNumber;
+  } else {
+    errorMsgPhoneElement.classList.remove("active");
+  }
 
-  db.map((item) => {
-    if (item.code === values.code) {
-      return;
-    } else if (item.code !== values.code) {
-      errors.code = "Podany kod jest nieprawidłowy";
-      if (codeErrorMsgWrapper === null) {
-        codeErrorMsgWrapper = inputCodeWrapper.createElement("div");
-        codeErrorMsgWrapper.innerHTML = `<p class="error-msg">${errors.code}</p>`;
-      } else {
-        codeErrorMsgWrapper.innerHTML = `<p class="error-msg">${errors.code}</p>`;
-      }
-    } else if (values.code.trim().length === 0) {
-      errors.code = "Proszę wprowadzić 4-cyfrowy kod odbioru";
-      if (codeErrorMsgWrapper === null) {
-        codeErrorMsgWrapper = inputCodeWrapper.createElement("div");
-        codeErrorMsgWrapper.innerHTML = `<p class="error-msg">${errors.code}</p>`;
-      } else {
-        codeErrorMsgWrapper.innerHTML = `<p class="error-msg">${errors.code}</p>`;
-      }
-    }
-  });
+  if (code.length !== 4) {
+    errors.code = "Wprowadź 4-cyfrowy kod odbioru";
+    errorMsgCodeElement.classList.add("active");
+    errorMsgCodeElement.innerText = errors.code;
+  } else if (isNaN(+code)) {
+    errorMsgCodeElement.classList.add("active");
+    errors.code = "Kod musi składać się z cyfr";
+    errorMsgCodeElement.innerText = errors.code;
+  } else {
+    errorMsgCodeElement.classList.remove("active");
+  }
+
+  if (
+    phoneNumber.length === 9 &&
+    !isNaN(+phoneNumber) &&
+    code.length === 4 &&
+    !isNaN(+code)
+  ) {
+    isInDatabase(phoneNumber, code);
+  }
+  //   errorMsgPhoneElement.classList.remove("active");
+  //   errorMsgCodeElement.classList.remove("active");
+  //   timerStop();
+  //   modalElement.classList.add("active");
+  // }
 };
 
 //Timer
@@ -130,17 +145,30 @@ const timerStop = () => {
   timerStarted = false;
 };
 
+const clearUp = () => {
+  inputPhoneElement.value = "";
+  inputCodeElement.value = "";
+  errorMsgCodeElement.innerText = "";
+  errorMsgPhoneElement.innerText = "";
+  errorMsgPhoneElement.classList.remove("active");
+  errorMsgCodeElement.classList.remove("active");
+};
+
 //Finish button handler
 const handleFinish = (e) => {
   e.preventDefault();
   modalElement.classList.remove("active");
+  mainElement.classList.add("active");
   formElement.classList.remove("active");
+  clearUp();
 };
 //Restart button handler
 const handleRestart = (e) => {
   e.preventDefault();
   modalElement.classList.remove("active");
-  timeAmount = 0;
+  mainElement.classList.add("active");
+  clearUp();
+  timerStart();
 };
 
 appInit();
